1429b0e0fe36f0df0e609bd551100e1771e452e9
oakes <zsoakes@gmail.com>
2016-05-07 09:06:23 -0400

    Start 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html-soup "1.1.1"
+(defproject html-soup "1.1.2-SNAPSHOT"
   :description "A library to add HTML tags to Clojure(Script) code"
   :url "https://github.com/oakes/html-soup"
   :license {:name "Public Domain"

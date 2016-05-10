815c443da97170ca078f3e0c8adedfac900951f7
oakes <zsoakes@gmail.com>
2016-05-09 21:09:01 -0400

    Update cljs and tag-soup

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.8.51"]
                  [prismatic/schema "0.4.3"]
-                 [tag-soup "1.2.2"]]
+                 [tag-soup "1.2.3"]]
   :profiles {:uberjar {:prep-tasks ["compile" ["cljsbuild" "once"]]}}
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :plugins [[lein-cljsbuild "1.1.2"]]

89ccd31314ba11f9d521a4ce99302c1eb3476c12
oakes <zsoakes@gmail.com>
2016-05-11 21:17:13 -0400

    Update tag-soup

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [prismatic/schema "0.4.3"]
-                 [tag-soup "1.3.1"]]
+                 [tag-soup "1.3.2"]]
   :profiles {:uberjar {:prep-tasks ["compile" ["cljsbuild" "once"]]}}
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :plugins [[lein-cljsbuild "1.1.2"]]

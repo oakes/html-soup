e7a46cb923b99421411f5912e2b61b43ba2c15ba
oakes <zach@sekao.net>
2019-02-15 12:58:20 -0600

    Use clj to deploy instead of boot

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject html-soup "1.6.1-SNAPSHOT"
+  :description "A library to add HTML tags to Clojure(Script) code"
+  :url "https://github.com/oakes/html-soup"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/UNLICENSE"}
+  :plugins [[lein-tools-deps "0.4.3"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :main html-soup.core)

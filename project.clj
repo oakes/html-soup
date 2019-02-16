5410d31badc5e3acf2286cfd908faa74cfad0f46
oakes <zach@sekao.net>
2019-02-16 16:49:53 -0600

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html-soup "1.6.1-SNAPSHOT"
+(defproject html-soup "1.6.1"
   :description "A library to add HTML tags to Clojure(Script) code"
   :url "https://github.com/oakes/html-soup"
   :license {:name "Public Domain"

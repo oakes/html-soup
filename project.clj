c5c367f122a1f6d237c8e27d35ca5c0cbd9fb6eb
oakes <zach@sekao.net>
2020-09-14 09:56:02 -0400

    Start 2.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html-soup "2.0.0"
+(defproject html-soup "2.1.0-SNAPSHOT"
   :description "A library to add HTML tags to Clojure(Script) code"
   :url "https://github.com/oakes/html-soup"
   :license {:name "Public Domain"

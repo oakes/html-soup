e25b2aae512b5b2f9ff8308dab8b36c86500c7e4
oakes <zach@sekao.net>
2020-09-16 08:23:46 -0400

    Add exclusions

--- dev.clj
+++ dev.clj
@@ -2,6 +2,9 @@
   '[figwheel.main :as figwheel]
   '[dynadoc.core :as dynadoc])
 
-(dynadoc/-main "--port" "5000")
+(dynadoc/start {:port 5000
+                :exclusions '#{html-soup.core/escape-html-str
+                               html-soup.core/node->html
+                               html-soup.core/node->hiccup}})
 (figwheel/-main "--build" "dev")
 
